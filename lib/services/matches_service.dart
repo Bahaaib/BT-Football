6a7136d6b6c33a5da76d4892feade6fed6919e12
bahaaib <bahaasco@gmail.com>
2022-02-16 17:20:29 +0200

    refactor(service): add function header to calculate a competition start date based on a given period

--- lib/services/matches_service.dart
+++ lib/services/matches_service.dart
@@ -1,6 +1,10 @@
 import 'package:bt_football/models/exports.dart';
 
 class MatchesService {
+  DateTime _calculateStartDate(
+          {required DateTime endDate, required int period}) =>
+      throw UnimplementedError();
+
   bool _competitionEnded(DateTime? endDate) => throw UnimplementedError();
 
   Future<Team?> _findTopTeam({required int topTeamId}) async =>
