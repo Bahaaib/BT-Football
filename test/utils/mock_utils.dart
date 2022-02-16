cba6e32e088d073ad468bf602494f34745ea08b9
bahaaib <bahaasco@gmail.com>
2022-02-16 19:26:47 +0200

    test(utils): add functions to load all mock assets for other tests

--- test/utils/mock_utils.dart
+++ test/utils/mock_utils.dart
@@ -3,6 +3,18 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 
 class MockUtil {
+  static Future<Map<String, dynamic>> loadMatchesJson() async {
+    return await _loadJson(path: 'assets/json/matches_response_mock.json');
+  }
+
+  static Future<Map<String, dynamic>> loadCompetitionJson() async {
+    return await _loadJson(path: 'assets/json/competition_response_mock.json');
+  }
+
+  static Future<Map<String, dynamic>> loadTeamJson() async {
+    return await _loadJson(path: 'assets/json/team_response_mock.json');
+  }
+
   static Future<Map<String, dynamic>> _loadJson({required String path}) async {
     final String mockResponse = await rootBundle.loadString(path);
 
