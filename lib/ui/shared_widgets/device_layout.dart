479943d197c46a801532db7f7025ed0867dd26f4
bahaaib <bahaasco@gmail.com>
2022-02-17 01:35:05 +0200

    feat(orientation): add device layout controller view

---
+++ lib/ui/shared_widgets/device_layout.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:bt_football/resources/dimens.dart';
+
+class DeviceLayout extends StatelessWidget {
+  final Widget onTablet;
+  final Widget onMobile;
+  final double mobileMinThreshold;
+
+  const DeviceLayout(
+      {Key? key,
+        required this.onTablet,
+        required this.onMobile,
+        this.mobileMinThreshold = AppDimens.mobileWidthThreshold})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isTablet()) {
+      return onTablet;
+    }
+    return onMobile;
+  }
+
+  bool _isTablet() {
+    final MediaQueryData data =
+    MediaQueryData.fromWindow(WidgetsBinding.instance!.window);
+    return data.size.shortestSide > mobileMinThreshold;
+  }
+}
---
+++ lib/ui/shared_widgets/orientation_layout.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class OrientationLayout extends StatelessWidget {
+  final Widget onPortrait;
+  final Widget onLandscape;
+
+  const OrientationLayout(
+      {Key? key, required this.onPortrait, required this.onLandscape})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OrientationBuilder(
+        builder: (BuildContext context, Orientation orientation) {
+          if (orientation == Orientation.portrait) {
+            return onPortrait;
+          }
+          return onLandscape;
+        });
+  }
+}
