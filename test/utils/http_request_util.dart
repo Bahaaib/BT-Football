dc0b6c3603dc3f4ae4617eab27be68bf67b1a597
bahaaib <bahaasco@gmail.com>
2022-02-16 19:34:22 +0200

    test(utils): add mock http client adapter to inject mock responses

--- test/utils/http_request_util.dart
+++ test/utils/http_request_util.dart
@@ -1,1 +1,13 @@
-class RequestUtil {}
+import 'package:dio/dio.dart';
+import 'package:http_mock_adapter/http_mock_adapter.dart';
+
+class RequestUtil {
+  final Dio _dio = Dio();
+  late DioAdapter _dioAdapter;
+
+  Dio get mockHttpClient {
+    _dioAdapter = DioAdapter(dio: _dio);
+    _dio.httpClientAdapter = _dioAdapter;
+    return _dio;
+  }
+}
