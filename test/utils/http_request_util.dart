26241e33c1d547b378eb6c8f211461e8704e0f4b
bahaaib <bahaasco@gmail.com>
2022-02-16 19:35:08 +0200

    test(utils): add http response mock function

--- test/utils/http_request_util.dart
+++ test/utils/http_request_util.dart
@@ -10,4 +10,19 @@ class RequestUtil {
     _dio.httpClientAdapter = _dioAdapter;
     return _dio;
   }
+
+  void _mockHttpRequest(
+      {required String path,
+        required Map<String, dynamic> response,
+        Map<String, dynamic>? queryParams}) {
+    _dioAdapter.onGet(path, (request) async {
+      request.reply(200, response);
+    },
+        data: null,
+        headers: {
+          "X-Auth-Token": "73088ac0ab05426081a1810b71f81e7d",
+          "content-type": "application/json; charset=utf-8"
+        },
+        queryParameters: queryParams);
+  }
 }
