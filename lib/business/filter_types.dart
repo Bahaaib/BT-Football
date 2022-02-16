8dbf96f89355b92a81fe8f99f3095f0cfc1f9b64
bahaaib <bahaasco@gmail.com>
2022-02-16 18:14:30 +0200

    refactor(filters): add a most winning filter critiria

---
+++ lib/business/filter_types.dart
@@ -0,0 +1,10 @@
+import 'package:bt_football/business/filter.dart';
+import 'package:bt_football/models/match.dart';
+
+class MostWinning extends Filter {
+  @override
+  List<int?>? getTeamIdsByMatchResult({required Match match}) {
+    // TODO: implement getTeamIdsByMatchResult
+    throw UnimplementedError();
+  }
+}
