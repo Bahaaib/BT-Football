2e640fb8a64f08508df9da5812785412529ac0a9
bahaaib <bahaasco@gmail.com>
2022-02-16 21:25:37 +0200

    test(service): init service test

---
+++ test/matches_service_test.dart
@@ -0,0 +1,27 @@
+import 'package:bt_football/network/layers/network_connectivity.dart';
+import 'package:bt_football/network/layers/network_performer.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mockito/annotations.dart';
+
+import 'matches_service_test.mocks.dart';
+import 'utils/http_request_util.dart';
+
+@GenerateMocks([NetworkConnectivity])
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  final RequestUtil _requestsUtil = RequestUtil();
+
+  void _injectDependencies() {
+    GetIt.instance.registerSingleton<Dio>(_requestsUtil.mockHttpClient);
+    GetIt.instance.registerLazySingleton<NetworkConnectivity>(
+        () => MockNetworkConnectivity());
+    GetIt.instance
+        .registerLazySingleton<NetworkPerformer>(() => NetworkPerformer());
+  }
+
+  setUpAll(() async {
+    _injectDependencies();
+  });
+}
