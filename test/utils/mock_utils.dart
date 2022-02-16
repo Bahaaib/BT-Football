c58f165a5c79daa9010be95354414a4e8ffdbbc5
bahaaib <bahaasco@gmail.com>
2022-02-16 19:31:51 +0200

    test(utils): add mock network connectivity utils

--- test/utils/mock_utils.dart
+++ test/utils/mock_utils.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
 
+import 'package:bt_football/network/layers/network_connectivity.dart';
 import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mockito/mockito.dart';
 
 class MockUtil {
   static Future<Map<String, dynamic>> loadMatchesJson() async {
@@ -19,5 +22,20 @@ class MockUtil {
     final String mockResponse = await rootBundle.loadString(path);
 
     return json.decode(mockResponse);
+  }
+
+  static void setNetworkConnected() {
+    _mockNetworkConnectivity(connected: true);
+  }
+
+  static void setNetworkDisconnected() {
+    _mockNetworkConnectivity(connected: false);
+  }
+
+  static void _mockNetworkConnectivity({required bool connected}) {
+    final NetworkConnectivity networkConnectivity =
+        GetIt.instance<NetworkConnectivity>();
+
+    when(networkConnectivity.connected).thenAnswer((_) async => connected);
   }
 }
