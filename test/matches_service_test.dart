2f452d4eaa095981562969713861ee6155aa80a8
bahaaib <bahaasco@gmail.com>
2022-02-16 22:15:52 +0200

    test(service): add test to fetch all matches from mock response

--- test/matches_service_test.dart
+++ test/matches_service_test.dart
@@ -1,12 +1,17 @@
 import 'package:bt_football/network/layers/network_connectivity.dart';
 import 'package:bt_football/network/layers/network_performer.dart';
+import 'package:bt_football/network/network_models/network_error.dart';
+import 'package:bt_football/resources/strings.dart';
+import 'package:bt_football/services/matches_service.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mockito/annotations.dart';
+import 'package:bt_football/models/exports.dart';
 
 import 'matches_service_test.mocks.dart';
 import 'utils/http_request_util.dart';
+import 'utils/mock_utils.dart';
 
 @GenerateMocks([NetworkConnectivity])
 void main() {
@@ -23,6 +28,30 @@ void main() {
 
   setUpAll(() async {
     _injectDependencies();
+  });
+
+  group('matches service class', () {
+    test(
+        'when fetching competition matches while network is connected,'
+            ' should get list of matches', () async {
+      MockUtil.setNetworkConnected();
+      List<Match> matches = await MatchesService()
+          .fetchCompetitionMatches(CodeStrings.englishPremierLeague);
+
+      expect(matches.isNotEmpty, true);
+    });
+
+    test(
+        'when fetching competition matches while network is connected,'
+            ' should throw a network error', () async {
+      MockUtil.setNetworkDisconnected();
+
+      expect(
+              () async => MatchesService()
+              .fetchCompetitionMatches(CodeStrings.englishPremierLeague),
+          throwsA(isA<NetworkError>()));
+    });
+
   });
 
   tearDownAll(() => GetIt.instance.reset());
