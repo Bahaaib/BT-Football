91f3a145a9080b0576a4932ae23de7e81ebf1d99
bahaaib <bahaasco@gmail.com>
2022-02-16 18:10:30 +0200

    refactor(filters): add a function header to sort the results of matches descendingly

--- lib/business/filter.dart
+++ lib/business/filter.dart
@@ -1,6 +1,9 @@
 import 'package:bt_football/business/filterable.dart';
 
 abstract class Filter implements Filterable {
+  Map<int, int> _sortResultsOfPlayedMatches(Map<int, int> unsortedResults) =>
+      throw UnimplementedError();
+
   Map<int, int> _extractResultsFromPlayedMatches(List<Match> playedMatches) =>
       throw UnimplementedError();
 }
