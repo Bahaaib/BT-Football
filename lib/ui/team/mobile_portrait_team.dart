7c2e4263992a4bcbb94ebb16bfbe285cd90a6e4b
bahaaib <bahaasco@gmail.com>
2022-02-17 01:31:56 +0200

    feat(orientation): add portrait orientation view for tablets

---
+++ lib/ui/team/mobile_portrait_team.dart
@@ -0,0 +1,30 @@
+import 'package:bt_football/resources/dimens.dart';
+import 'package:flutter/material.dart';
+import 'package:bt_football/ui/team/base_team.dart';
+
+class MobilePortraitTeam extends BaseTeam {
+  const MobilePortraitTeam(
+      {Key? key,
+      required Widget squad,
+      required Widget teamInfo,
+      EdgeInsets? margin =
+          const EdgeInsets.symmetric(horizontal: AppDimens.screenPadding),
+      EdgeInsets? padding})
+      : super(
+            key: key,
+            squad: squad,
+            teamInfo: teamInfo,
+            margin: margin,
+            padding: padding);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: margin,
+      padding: padding,
+      child: Column(
+        children: [teamInfo, Flexible(child: squad)],
+      ),
+    );
+  }
+}
