f4fa1e5a9b4d6d55912c5c8c49d99c1c24d27a18
bahaaib <bahaasco@gmail.com>
2022-02-16 20:47:12 +0200

    refactor(filters): implement most losing filtering criteria

--- lib/business/filter_types.dart
+++ lib/business/filter_types.dart
@@ -16,8 +16,10 @@ class MostWinning extends Filter {
 class MostLosing extends Filter {
   @override
   List<int?>? getTeamIdsByMatchResult({required Match match}) {
-    // TODO: implement getTeamIdsByMatchResult
-    throw UnimplementedError();
+    final Score? score = match.score;
+    if (score == null || score.winner == CodeStrings.draw) return null;
+    if (score.winner == CodeStrings.homeTeam) return [match.awayTeam?.id];
+    return [match.homeTeam?.id];
   }
 }
 
