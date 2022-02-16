e7d0799ca65405b668e87ffcd18ab306673865d3
bahaaib <bahaasco@gmail.com>
2022-02-16 18:12:03 +0200

    refactor(filters): add a function header to find the top team id

--- lib/business/filter.dart
+++ lib/business/filter.dart
@@ -1,6 +1,9 @@
 import 'package:bt_football/business/filterable.dart';
 
 abstract class Filter implements Filterable {
+  int findTopTeamIdFor({required List<Match> playedMatches}) =>
+      throw UnimplementedError();
+
   Map<int, int> _sortResultsOfPlayedMatches(Map<int, int> unsortedResults) =>
       throw UnimplementedError();
 
