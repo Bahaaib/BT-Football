c5960ca637acfcd3a5354380e38fcc393dc011a0
bahaaib <bahaasco@gmail.com>
2022-02-16 17:18:50 +0200

    refactor(service): add function header to decides if a competition has ended

--- lib/services/matches_service.dart
+++ lib/services/matches_service.dart
@@ -1,6 +1,8 @@
 import 'package:bt_football/models/exports.dart';
 
 class MatchesService {
+  bool _competitionEnded(DateTime? endDate) => throw UnimplementedError();
+
   Future<Team?> _findTopTeam({required int topTeamId}) async =>
       throw UnimplementedError();
 }
