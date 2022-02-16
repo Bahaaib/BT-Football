784958c4d735164e06e10da1958555195914b4f1
bahaaib <bahaasco@gmail.com>
2022-02-16 23:54:38 +0200

    test(home_page): add widget tester function to run different scenarios

--- test/home_page_test.dart
+++ test/home_page_test.dart
@@ -1,7 +1,12 @@
 import 'package:bt_football/bloc/matches/matches_bloc.dart';
+import 'package:bt_football/bloc/matches/matches_state.dart';
+import 'package:bt_football/ui/home/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 import 'home_page_test.mocks.dart';
 import 'utils/bloc_util.dart';
@@ -12,6 +17,28 @@ void main() {
 
   void _injectDependencies() {
     GetIt.instance.registerLazySingleton<MatchesBloc>(() => MockMatchesBloc());
+  }
+
+  Future<void> _testWidget(
+      {required WidgetTester tester,
+      required MatchesState givenState,
+      required Type expectedWidget}) async {
+    final MatchesBloc mockMatchesBloc = GetIt.instance<MatchesBloc>();
+    when(mockMatchesBloc.state).thenAnswer((_) => givenState);
+
+    Widget widget = MaterialApp(
+      builder: (_, __) => BlocProvider<MatchesBloc>(
+        create: (_) => mockMatchesBloc,
+        child: const HomePage(),
+      ),
+    );
+
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(widget);
+
+    final Finder homePage = find.byType(expectedWidget);
+
+    expect(homePage, findsOneWidget);
   }
 
   setUpAll(() async {
