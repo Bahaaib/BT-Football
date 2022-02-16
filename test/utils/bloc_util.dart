4c16906ea21406c6dd1e3eb921a2569db4c6cc89
bahaaib <bahaasco@gmail.com>
2022-02-16 19:49:37 +0200

    test(utils): init bloc mock util

---
+++ test/utils/bloc_util.dart
@@ -0,0 +1,17 @@
+import 'package:bt_football/bloc/matches/exports.dart';
+import 'package:bt_football/models/exports.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mockito/mockito.dart';
+
+class BlocUtil {
+  static void stubMatchesBloc() {
+    final MatchesBloc matchesBloc = GetIt.instance<MatchesBloc>();
+    final Team simpleTeam = Team(name: 'Liverpool FC', foundationYear: 1892);
+
+    when(matchesBloc.stream).thenAnswer(
+      (_) => Stream.fromIterable(
+        [MatchesInitial()],
+      ),
+    );
+  }
+}
