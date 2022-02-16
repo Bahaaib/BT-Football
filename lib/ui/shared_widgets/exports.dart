57db2fab8356f2512c17cb577115e9aa2287a794
bahaaib <bahaasco@gmail.com>
2022-02-17 01:39:48 +0200

    refactor(home_page): export shared widgets files paths

---
+++ lib/ui/shared_widgets/exports.dart
@@ -0,0 +1,5 @@
+export 'package:bt_football/ui/shared_widgets/empty_widget.dart';
+export 'package:bt_football/ui/shared_widgets/error_widget.dart';
+export 'package:bt_football/ui/shared_widgets/loading_widget.dart';
+export 'package:bt_football/ui/shared_widgets/orientation_layout.dart';
+export 'package:bt_football/ui/shared_widgets/device_layout.dart';
